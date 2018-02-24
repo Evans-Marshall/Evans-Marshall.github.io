262502a0a5bfc6e6d858e59e7eabf0f480c9b485
Marshall <32920499+YungMoose@users.noreply.github.com>
2018-02-24 13:11:19 -0700

    Lesson 7
    * removed float alignment in styling and replaced with flex or grid
    * added Gallery page with pics and stylesheets

---
+++ assignments/lesson7/js/windchill.js
@@ -0,0 +1,6 @@
+document.getElementById("output").innerHTML = output;
+}
+	function windChill(75, 10){
+	var howcold = 35.74 + (.6215 * temp) - (35.75*(Math.pow(mph,.16))) + (.4275 * temp * (Math.pow(mph,.16)));
+	return output;
+	}
